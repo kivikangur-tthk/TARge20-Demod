66170dd58df54c72c4b454f5fcc0e5581f8198db
kristjankivikangur <kkivik@ttu.ee>
2021-01-14 16:43:39 +0200

    Massiiv

--- KlassimuutujaDemo/KlassimuutujaDemo/Program.cs
+++ KlassimuutujaDemo/KlassimuutujaDemo/Program.cs
@@ -8,8 +8,15 @@ namespace KlassimuutujaDemo
 		{
 			Koer muri = new Koer("Muri", "Krants", true, 5.5f);
 			muri.KuvaAndmed();
+			Koer kuri = muri;
+			kuri.KuvaAndmed();
 			Koer bella = new Koer("Bella", "Puudel", false, 2.1f);
 			bella.KuvaAndmed();
+			Koer[] kennel = new Koer[10];
+			for (int i = 0; i < kennel.Length; i++)
+			{
+				kennel[i] = new Koer($"Muki{i}", "Krants", true, i);
+			}
 			Koer.KuvaStatistika();
 		}
 	}
