af21477f956ca3ec1cefae75a74b16b8db7a2938
kristjankivikangur <kkivik@ttu.ee>
2021-01-14 16:27:48 +0200

    Klassimuutujademo

---
+++ KlassimuutujaDemo/KlassimuutujaDemo/Koer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KlassimuutujaDemo
+{
+	class Koer
+	{
+		private string _nimi;
+		private string _toug;
+		private bool _onIsane;
+		private float _kaal;
+		private static int _kokku = 0;
+		private static int _isaseid = 0;
+		private static float _koguKaal = 0;
+
+		public Koer(string nimi, string toug, bool onIsane, float kaal)
+		{
+			_nimi = nimi;
+			_toug = toug;
+			_onIsane = onIsane;
+			_kaal = kaal;
+			_kokku++;
+			_isaseid += onIsane ? 1 : 0;
+			_koguKaal += kaal;
+			//if (onIsane)	_isaseid++;
+		}
+
+		public void KuvaAndmed()
+		{
+			Console.WriteLine($"Nimi: {_nimi}; Tõug: {_toug}; Sugu: {(_onIsane?"isane":"emane")}; Kaal: {_kaal}.");
+		}
+		public static void KuvaStatistika()
+		{
+			Console.WriteLine($"Koeri: {_kokku}; Isaseid: {_isaseid}; Emaseid: {_kokku-_isaseid}; Kogu kaal: {_koguKaal}");
+		}
+	}
+}
---
+++ KlassimuutujaDemo/KlassimuutujaDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace KlassimuutujaDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Koer muri = new Koer("Muri", "Krants", true, 5.5f);
+			muri.KuvaAndmed();
+			Koer bella = new Koer("Bella", "Puudel", false, 2.1f);
+			bella.KuvaAndmed();
+			Koer.KuvaStatistika();
+		}
+	}
+}
