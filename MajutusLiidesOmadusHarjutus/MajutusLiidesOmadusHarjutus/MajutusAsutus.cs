7dd3dd6b8e0c429d0b6aa4b6236b376eedd3217f
kristjankivikangur <kkivik@ttu.ee>
2021-02-11 19:14:20 +0200

    Majutusasutus Omadus tehtud

---
+++ MajutusLiidesOmadusHarjutus/MajutusLiidesOmadusHarjutus/MajutusAsutus.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MajutusLiidesOmadusHarjutus
+{
+	class MajutusAsutus
+	{
+		protected decimal _hind;
+		protected int _kohti;
+		protected int _kylalisi;
+		public MajutusAsutus(decimal hind,int kohti)
+		{
+			_hind = hind;
+			_kohti = kohti;
+			_kylalisi = 0;
+		}
+		public int VabuKohti { 
+			get
+			{
+				return _kohti - _kylalisi;
+			} 
+			set
+			{
+				if (value < 0 || _kohti < value)		// vabukohti võimalik 0 kuni kohti
+				{
+					return;
+				}
+				_kylalisi = _kohti - value;
+			}
+		}
+		public int ArvutaMaksumus(int külalisi)
+		{
+			//1.Kui külalisi on rohkem kui vabu kohti annab veateate
+			if (külalisi>VabuKohti)
+			{
+				throw new ArgumentOutOfRangeException(nameof(külalisi), "Külalisi ei tohi olla rohkem kui vabu kohti");
+			}
+			//2.Kui külalisi on negatiivne arv annab veateate
+			if (külalisi<0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(külalisi), "Külalisi ei tohi olla negatiivne arv");
+			}
+			//3.Vähendab vabu kohti külaliste arvu võrra(kui eelmised kaks ei andnud viga)
+			VabuKohti -= külalisi;
+			//4.Tagastab mitu tuba hõivatakse(praegu alati 1)
+			return 1;
+		}
+	}
+}
---
+++ MajutusLiidesOmadusHarjutus/MajutusLiidesOmadusHarjutus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MajutusLiidesOmadusHarjutus
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
