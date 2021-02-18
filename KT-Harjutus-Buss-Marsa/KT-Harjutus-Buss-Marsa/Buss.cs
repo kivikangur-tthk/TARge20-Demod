4c3a071720db05fdad462d8b3b635ddcf5f33f85
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2021-02-18 19:36:00 +0200

    Loo Buss ning lisa väljad ja meetodid.

---
+++ KT-Harjutus-Buss-Marsa/KT-Harjutus-Buss-Marsa/Buss.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KT_Harjutus_Buss_Marsa
+{
+	class Buss
+	{
+		protected string _värv;
+		protected int _kohtadeArv;
+		protected string _mudel;
+		protected int _aasta;
+		protected int _reisijaid;
+
+		public Buss(string värv, int kohtadeArv,string mudel,int aasta)
+		{
+			_värv = värv;
+			_kohtadeArv = kohtadeArv;
+			_mudel = mudel;
+			_aasta = aasta;
+			_reisijaid = 0;
+		}
+		public void KuvaAndmed()
+		{
+			Console.WriteLine($"Mudel: {_mudel}\nVärv: {_värv}\nKohti/Reisijaid: {_kohtadeArv}/{_reisijaid}\nAasta: {_aasta}");
+			
+		}
+		public void KuvaVanusAastal2030()
+		{
+			Console.WriteLine($"Bussi vanus aastal 2030: {2030-_aasta}");
+		}
+		public void LaeReisijaid(int kogus)
+		{
+			if (kogus > _kohtadeArv-_reisijaid || kogus+_reisijaid < 0)
+			{
+				return;
+			}
+			_reisijaid += kogus;
+		}
+	}
+}
---
+++ KT-Harjutus-Buss-Marsa/KT-Harjutus-Buss-Marsa/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace KT_Harjutus_Buss_Marsa
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Buss väikebuss = new Buss("Kollane", 5, "Ford", 2000);
+			väikebuss.KuvaAndmed();
+			väikebuss.KuvaVanusAastal2030();
+			väikebuss.LaeReisijaid(5);
+			väikebuss.KuvaAndmed();
+			väikebuss.LaeReisijaid(-5);
+			väikebuss.KuvaAndmed();
+		}
+	}
+}
