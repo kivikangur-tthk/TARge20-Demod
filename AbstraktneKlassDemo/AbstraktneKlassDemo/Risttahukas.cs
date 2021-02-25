754ef9ce03d454fbb951b4e21a4dba3f61bc9f35
kristjankivikangur <kkivik@ttu.ee>
2021-02-25 19:56:58 +0200

    Loo risttahuka klass

---
+++ AbstraktneKlassDemo/AbstraktneKlassDemo/Risttahukas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstraktneKlassDemo
+{
+	class Risttahukas : Kujund
+	{
+		public override double AnnaKõrgus()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override double AnnaPõhjaPindala()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override double AnnaPõhjaÜmbermõõt()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
